0df193d6efa89c592b72b105627d5033f7cc371a
Iku Iwasa <iku.iwasa@gmail.com>
2018-03-27 08:47:51 +0900

    Add package header and footer comments in ert test file

--- properties-mode-tests.el
+++ properties-mode-tests.el
@@ -1,4 +1,24 @@
-;;; -*- lexical-binding: t -*-
+;;; properites-mode-tests.el --- ERT tests for properties-mode.  -*- lexical-binding: t -*-
+
+;; Author: Iku Iwasa <iku.iwasa@gmail.com>
+;; URL: https://github.com/iquiw/properties-mode
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
 (require 'cl-lib)
 (require 'ert)
 
@@ -292,3 +312,6 @@
       (cl-letf (((symbol-function 'properties--maybe-encode-buffer) (lambda () (setq called t))))
         (revert-buffer nil t))
       (should (not called)))))
+
+(provide 'properties-mode-tests)
+;;; properties-mode-tests.el ends here
